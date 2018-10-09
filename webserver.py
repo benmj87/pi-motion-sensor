c136feebb5239907fbd10a3bfe557da2b9ad5a3d
Ben Johnston <ben@johnstonmail.co.uk>
2018-10-09 19:40:48 +0000

    Add handling of deletion/creation of file Add handling of deletion/creation of file

--- webserver.py
+++ webserver.py
@@ -1,3 +1,5 @@
+import os
+import os.path
 from pathlib import Path
 from http.server import BaseHTTPRequestHandler,HTTPServer
 
@@ -42,9 +44,16 @@ class myHandler(BaseHTTPRequestHandler):
 
   def do_POST(self):
     len = self.headers.get('content-length', 0)
-    body = self.rfile.read(int(len))
+    body = self.rfile.read(int(len)).decode('utf-8')
     print(body)
-
+   
+    if body == 'option=0':
+      if (os.path.exists('email-flag')):
+        os.remove('email-flag')
+    else:
+      with open('email-flag', 'w') as f:
+        f.write('nul')
+   
     self.send_response(301)
     self.send_header('Location','/')
     self.end_headers()
